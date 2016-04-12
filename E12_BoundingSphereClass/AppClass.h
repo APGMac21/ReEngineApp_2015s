1af75a61859a4f8c856b849a3afc3501b1d50f7e
Gregory T. McClellan (RIT Student) <gtm3063@ad.rit.edu>
2016-04-12 08:27:00 -0400

    no message

--- E12_BoundingSphereClass/AppClass.h
+++ E12_BoundingSphereClass/AppClass.h
@@ -15,6 +15,12 @@ class AppClass : public ReEngAppClass
 {
 	vector3 m_v3O1 = vector3(-2.5f, 0.0f, 0.0f);
 	vector3 m_v3O2 = vector3( 2.5f, 0.0f, 0.0f);
+
+	PrimitiveClass* m_pSphere1 = nullptr;
+	vector3 m_v3Center;
+
+	PrimitiveClass* m_pSphere2 = nullptr;
+	vector3 m_v3Center2;
 public:
 	typedef ReEngAppClass super;
 	/*
